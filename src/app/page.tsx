a8eccb9f5cd3f3ed5730a11d4bf439cf58c3f413
Felipe Pessoa de Sousa <66555960+bachgen-brazil@users.noreply.github.com>
2025-09-11 12:25:29 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             <div className="relative h-14 w-14 sm:h-16 sm:w-16">
               {/* Ajuste o caminho do logo aqui */}
               <Image
-                src="/logo-felipe.png"
+                src="/public/logo-felipe.png"
                 alt="Logo Felipe Pessoa"
                 fill
                 className="object-contain"
